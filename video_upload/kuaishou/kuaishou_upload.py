6f7e88457404cc85807a650c93dce157e4f85e4b
41525 <415256473@qq.com>
2024-06-06 15:00:45 +0800

    增加失败截图

--- video_upload/kuaishou/kuaishou_upload.py
+++ video_upload/kuaishou/kuaishou_upload.py
@@ -3,10 +3,12 @@ import base64
 import json
 import pathlib
 import random
+import uuid
 from datetime import datetime
 from io import BytesIO
 
 import loguru
+import pyautogui
 from PIL import Image
 from apscheduler.schedulers.blocking import BlockingScheduler
 from playwright.async_api import Playwright, async_playwright
@@ -72,6 +74,10 @@ async def kuaishou_cookie_gen(account_file):
         user_id = user_id.replace(" 用户 ID：", "").strip()
 
         user_name = await page.locator('.detail__name').text_content()
+        screenshot = pyautogui.screenshot()
+        # 将截图保存到指定路径
+        screenshot.save(f'/opt/software/auto_publish_videos/imgs/{uuid.uuid4()}.png')
+        print(user_id, user_name)
         # 点击调试器的继续，保存cookie
         await context.storage_state(path=get_account_file(user_id))
         try:
