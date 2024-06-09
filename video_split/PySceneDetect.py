172c0fcacc2632c492086c36f6e00131c5809ed0
41525 <415256473@qq.com>
2024-06-09 12:42:55 +0800

    分割时去除音频

--- video_split/PySceneDetect.py
+++ video_split/PySceneDetect.py
@@ -6,15 +6,16 @@ def split_video_into_scenes(video_path, output_path, threshold=27.0):
     # 打开我们的视频，创建场景管理器，并添加检测器.
     video = open_video(video_path)
     scene_manager = SceneManager()
-    scene_manager.add_detector(
-        ContentDetector(threshold=threshold))
+    scene_manager.add_detector(ContentDetector(threshold=threshold))
     scene_manager.detect_scenes(video, show_progress=True)
     scene_list = scene_manager.get_scene_list(start_in_scene=True)
-    split_video_ffmpeg(video_path, scene_list, output_path, '$VIDEO_NAME-$SCENE_NUMBER.mp4', show_progress=True)
+    split_video_ffmpeg(video_path, scene_list, output_path, '$VIDEO_NAME-$SCENE_NUMBER.mp4',
+                       arg_override='-map 0:v -c:v libx264 -preset veryfast -crf 22',
+                       show_progress=True)
     # scene_list = detect(video_path, ContentDetector())
     # split_video_ffmpeg(video_path, scene_list, output_path)
 
 
 if __name__ == '__main__':
-    split_video_into_scenes('/video/download/aa/66616a8a00000000060045f5.mp4',
-                            'D:/IDEA/workspace/auto_publish_videos/video/source/11')
+    split_video_into_scenes('D:/IDEA/workspace/auto_publish_videos/video/download/aa/11.mp4',
+                            'D:/IDEA/workspace/auto_publish_videos/video/source/')
--- video_split/main.py
+++ video_split/main.py
@@ -3,7 +3,7 @@ import time
 
 import loguru
 
-from merge import scheduled_job
+# from merge import scheduled_job
 from util.file_util import get_mp4_files_path
 from video_split.PySceneDetect import split_video_into_scenes
 
@@ -13,13 +13,23 @@ def split_video(folder_path, source_path):
     loguru.logger.info(f"分割视频需要处理的数据有{len(video_files)}条")
     last_time = time.time()
     for video_path in video_files:
-        video_folder = os.path.dirname(video_path)
-        output_folder = os.path.join(source_path, video_folder.replace(folder_path, ''))
-        split_video_into_scenes(video_path, output_folder)
-        # 每次运行完校验一下上次的运行时间，如果超过半个小时，则看一下有没有需要合并的数据
-        if last_time - time.time() > 1800:
-            scheduled_job()
-            last_time = time.time()
+        try:
+            video_folder = os.path.dirname(video_path)
+            output_folder = os.path.join(source_path, video_folder.replace(folder_path, ''))
+            loguru.logger.info(f'分割文件正在处理的文件为{video_path}')
+            split_video_into_scenes(video_path, output_folder)
+            # 每次运行完校验一下上次的运行时间，如果超过半个小时，则看一下有没有需要合并的数据
+            if last_time - time.time() > 1800:
+                # scheduled_job()
+                last_time = time.time()
+        except:
+            loguru.logger.info(f"分割视频失败 {video_path}: {e}")
+        finally:
+            if os.path.exists(video_path):
+                try:
+                    os.remove(video_path)
+                except PermissionError as e:
+                    loguru.logger.info(f"分割视频后删除视频失败 {video_path}: {e}")
 
 
 if __name__ == '__main__':
