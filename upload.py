5601bf0c7176a52fd7109e2e5c34edcaac041d45
41525 <415256473@qq.com>
2024-06-27 00:03:13 +0800

    修改配置

--- upload.py
+++ upload.py
@@ -26,6 +26,6 @@ if __name__ == '__main__':
         initial_execution_time = datetime.now().replace(hour=now.hour, minute=now.minute, second=now.second,
                                                         microsecond=0)
         # 使用 cron 规则指定每天23点执行一次
-        scheduler.add_job(upload_video, 'interval', hour=2, max_instances=1)  # 每30分钟执行一次
+        scheduler.add_job(upload_video, 'interval', hours=2, max_instances=1)  # 每30分钟执行一次
         scheduler.start()
     # upload_video()
